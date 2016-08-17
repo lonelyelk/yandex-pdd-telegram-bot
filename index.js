b932c7499d553eb040a410428ccc2757d33b2c5d
Sergey Kruk <sergey.kruk@gmail.com>
2016-08-17 23:54:27 +0300

    Rewrite almost from scratch starting with domains and DNS records.

--- index.js
+++ index.js
@@ -3,8 +3,18 @@
 	'use strict';
 
 	const config = require('./conf/config');
-	const bot = require('./lib/bot')({
+	const keyboard = require('./lib/keyboard');
+	const pdd = require('./lib/pdd')({
 		config: config
+	});
+	const dialogues = require('./lib/dialogues')({
+		keyboard: keyboard,
+		pdd: pdd
+	});
+
+	const bot = require('./lib/bot')({
+		config: config,
+		dialogues: dialogues
 	});
 	require('./lib/web')({
 		config: config,
