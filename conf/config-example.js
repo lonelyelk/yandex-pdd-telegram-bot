0965a8194db8b14e19ee5be4c62dee1dee1f11f1
Sergey Kruk <sergey.kruk@gmail.com>
2016-08-20 22:54:15 +0300

    Use different bot token for development.

--- conf/config-example.js
+++ conf/config-example.js
@@ -5,6 +5,7 @@
 		readonlyMail: ['admin@example.ru'],
 		domainMail: 'example.ru',
 		botToken: '123456',
+		devBotToken: '345678',
 		permitUsers: [], // telegram user id
 		port: 3000,
 		url: '/path/to/bot/api',
