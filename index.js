858db26357b869ce821b747cb64812ca8fd63f5d
Sergey Kruk <sergey.kruk@gmail.com>
2016-08-20 12:02:40 +0300

    Added DNS-records add/remove dialogues. More refactoring:
      * Graceful shutdown and startup with notification and keyboard
        reset for all listed permited users;
      * More const instead of let where needed;
      * Moved keyboard settings into static properties;
      * All errors are one time show only.

--- index.js
+++ index.js
@@ -16,8 +16,35 @@
 		config: config,
 		dialogues: dialogues
 	});
-	require('./lib/web')({
+	const server = require('./lib/web')({
 		config: config,
 		bot: bot
+	});
+
+	function gracefulClose() {
+		server.close(function () {
+			process.exit(0);
+		});
+	}
+
+	process.on('SIGINT', function () {
+		Promise.all(config.permitUsers.map(function (userId) {
+			return bot.sendMessage(userId, 'Бот временно выключается. Только спокойствие! 🐨', {
+				reply_markup: {
+					hide_keyboard: true
+				}
+			});
+		})).then(gracefulClose).catch(function (err) {
+			console.log(err);
+			gracefulClose();
+		});
+	});
+
+	config.permitUsers.map(function (userId) {
+		return bot.sendMessage(userId, 'Бот снова в строю. Скажите что-нибудь! 🐯', {
+			reply_markup: {
+				hide_keyboard: true
+			}
+		});
 	});
 })();
